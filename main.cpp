0072a7e0c78924e59ff1d10616c2d1292171a883
NameYef <jeffoee12345@gmail.com>
2023-11-14 11:33:34 +0800

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,43 @@
 #include "manager.h"
 #include "board.h"
+#include "ncurses.h"
 
-// testing board initiation and drawing
-int main() {
+// Initialize ncurses standard screen, start colour
+void game_init(){
+  setlocale(LC_ALL, ""); // set locale to draw unicode characters
+  initscr();             // Initialize the window
+
+  // start colour if its supported
+  if(has_colors() == FALSE)
+  {
+    endwin();
+    printf("Your terminal does not support color\n");
+    exit(1);
+  }
+
+  start_color();		    	/* Start color 			*/
+
+  raw();			           	/* Line buffering disabled	*/
+  keypad(stdscr, TRUE);		/* We get F1, F2 , arrow keys etc..		*/
+  noecho();               // Don't echo any keypresses
+  curs_set(FALSE);        // Don't display a cursor
+  nodelay(stdscr, true);  //allow to animate screen while waiting input
+}
+
+
+int main(int argc, char ** argv) {
+
+    game_init();
     Manager* manager = new Manager();
 
     while (true) {
-        manager->run();
-       
+
+    manager->run();
+    
     }
 
     delete manager;
+    endwin();
     return 0;
 
 }
