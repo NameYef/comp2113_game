87c4f58c1957098876893d237af5e281ce569946
Lei Kam Fai <97010099+CashonPierre@users.noreply.github.com>
2023-11-19 23:56:30 +0800

    init title when start running game

---
+++ title.cpp
@@ -0,0 +1,46 @@
+#include <ncurses.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+void start_title() {
+    int yMax, xMax;
+    //get screen size
+    getmaxyx(stdscr, yMax, xMax);
+
+    //erase screen
+    erase();
+    //init attribtion
+    attrset(0);
+    //set color
+    init_pair(12, COLOR_RED, COLOR_RED);
+    init_pair(13, COLOR_GREEN, COLOR_BLACK);
+    //turn on color
+    attron(COLOR_PAIR(12));
+    //target title
+    vector<string> titlename;
+    titlename.push_back("###   #  ### ### #   ### ### #  # ### ### ");
+    titlename.push_back("#  # # #  #   #  #   #   #   #  #  #  #  #");
+    titlename.push_back("###  ###  #   #  #   ### ### ####  #  ### ");
+    titlename.push_back("#  # # #  #   #  #   #     # #  #  #  #   ");
+    titlename.push_back("###  # #  #   #  ### ### ### #  # ### #   ");
+
+    //set row and column of the place of title
+    int r = yMax / 2 - 9, c = (xMax - 42) / 2;
+    //loop out all element with respected color
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 42; j++) {
+            char tmp = titlename[i][j];
+            if (tmp == ' ') {
+                mvaddch(r, c, tmp);
+            } else {
+                attron(COLOR_PAIR(12));
+                mvaddch(r, c, tmp);
+                attroff(COLOR_PAIR(12));
+            }
+            c++;
+        }
+        c = (xMax - 42) / 2;
+        r++;
+    }
+}
