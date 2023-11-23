6d68f3f9f0a616196b4fb050d305e9d36e3e48e3
Lei Kam Fai <97010099+CashonPierre@users.noreply.github.com>
2023-11-24 00:47:29 +0800

    Create start_title.h

---
+++ start_title.h
@@ -0,0 +1,9 @@
+// start_title.h
+
+#ifndef START_TITLE_H  // Include guard to prevent multiple inclusion
+#define START_TITLE_H
+
+// Declaration of the start_title function
+void start_title();
+
+#endif  // End of include guard
