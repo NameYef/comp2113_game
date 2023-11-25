0d2ebe2e83ae0e4d0a6034b2756e9dcb4fe5d8ae
nshb765 <145572145+nshb765@users.noreply.github.com>
2023-11-25 11:16:19 +0800

    Update player.h

--- player.h
+++ player.h
@@ -10,14 +10,15 @@ public:
     void setup();
     
     // method for gameplay
-
+    void gameplay(int& input_x, int& input_y, char& direction);
 
 private:
     void state_set(char& direction, int& head_x, int& head_y, int& no_of_blocks, int state_no);
     void ship_set(char direction, int current_ship, int head_x, int head_y, int no_of_blocks);
     bool overlap(char& direction, int& head_x, int& head_y, int& no_of_blocks);
 
-
+    void state_update(int& input_x, int& input_y);
+    void ship_specific(int& input_x, int& input_y, char& direction);
 
 
 };
