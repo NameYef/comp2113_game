bd4bb3ded412aa9c7ca3fbcfc41e2555c53775a1
Zhumabayeva Aisha <aisha@academy21.cs.hku.hk>
2023-11-29 14:15:21 +0800

    added comments to bot_attack function

--- player.h
+++ player.h
@@ -12,15 +12,16 @@ public:
     
     // method for gameplay
     void bot_attack();
-    void bot_at_0(mt19937& gen);
-    void bot_at_1(mt19937& gen);
-    void bot_at_2(mt19937& gen);
-    void bot_at_3(mt19937& gen);
-    void bot_at_4(mt19937& gen);
-    void bot_at_neg1(mt19937& gen);
-    void bot_at_neg2(mt19937& gen);
-    void bot_at_neg3(mt19937& gen);
-    void bot_at_neg4(mt19937& gen);
+    //methods used in bot_attack function
+    void bot_attempts_0(mt19937& gen);
+    void bot_attempts_1(mt19937& gen);
+    void bot_attempts_2(mt19937& gen);
+    void bot_attempts_3(mt19937& gen);
+    void bot_attempts_4(mt19937& gen);
+    void bot_attempts_neg1(mt19937& gen);
+    void bot_attempts_neg2(mt19937& gen);
+    void bot_attempts_neg3(mt19937& gen);
+    void bot_attempts_neg4(mt19937& gen);
 
 private:
     void state_set(char& direction, int& head_x, int& head_y, int& no_of_blocks, int state_no);
@@ -30,9 +31,14 @@ private:
     bool bot_overlap(int x, int y);
     void hit(int cursor_x, int cursor_y);
 
+    //bot_attempts variable is needed for giving directions to bot in bot_attack function
     int bot_attempts = 0;
+    //coordinate_x and coordinate_y are used by bot_attack function if it has located a cell with a part of the ship inside
     int coordinate_x = 0;
     int coordinate_y = 0;
+    // original_x and original_y coordinates are used in bot_attack function to store
+    // the coordinates of x and y when bot_attack finds at least 1 cell with a part of the ship inside
+    // original_x and original_y will store the coordinates of that cell
     int original_x = 0;
     int original_y = 0;
 
